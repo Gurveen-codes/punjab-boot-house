f57212f4b452c1c4abcc9e693c5d51fd4f4781c0
Gurveen <gurveens11@gmail.com>
2021-01-23 14:19:46 +0530

    Setup react-router and react-router-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <HomeScreen></HomeScreen>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
         </Container>
       </main>
       <Footer></Footer>
-    </>
+    </Router>
   );
 };
 

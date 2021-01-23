35de9d1eb1d16f4bdeb519d239a126d5ee54437c
Gurveen <gurveens11@gmail.com>
2021-01-23 23:21:06 +0530

    Setup mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Backend API is running");

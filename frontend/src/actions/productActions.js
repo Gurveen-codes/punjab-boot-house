fe3be3ae92432fa24e37b18239503d040b3c1343
Gurveen <gurveens11@gmail.com>
2021-01-24 17:53:32 +0530

    setup product list reducer and actions

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import * as actionTypes from "../constants/actionTypes.js";
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: actionTypes.PRODUCT_LIST_REQUEST });
+
+    const { data } = await axios.get("/api/products");
+
+    dispatch({ type: actionTypes.PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,14 @@
+import * as actionTypes from "../constants/actionTypes.js";
+
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case actionTypes.PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
+    case actionTypes.PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    case actionTypes.PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};

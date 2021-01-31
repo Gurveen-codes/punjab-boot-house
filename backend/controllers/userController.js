e541eeeb5ac0df82ffcd6ce76ecae93801bb4977
Gurveen <gurveens11@gmail.com>
2021-01-31 17:34:11 +0530

    [B]: Send User Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,5 +28,16 @@ export const authUser = asyncHandler(async (req, res) => {
 // @route /api/users/profile
 // @access Private
 export const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("Success");
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
 });

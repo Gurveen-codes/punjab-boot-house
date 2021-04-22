a43adff6ff8d255ca29a0b249c8e5e903b361572
Gurveen <gurveens11@gmail.com>
2021-04-22 17:04:19 +0530

    [B]: Add route to update and add new product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -37,4 +37,54 @@ const deleteProduct = asyncHandler(async (req, res) => {
 	}
 });
 
-export { getProductById, getProducts, deleteProduct };
+// @desc Create a product
+// @route POST /api/products/
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: "Sample Name",
+		image: "/images/sample.jpg",
+		description: "Sample Description",
+		brand: "Sample Brand",
+		category: "Sample Category",
+		price: 0,
+		countInStock: 0,
+		user: req.user,
+	});
+
+	const createdProduct = await product.save();
+
+	res.status(201).json(createdProduct);
+});
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		product.name = req.body.name || product.name;
+		product.price = req.body.price || product.price;
+		product.image = req.body.image || product.image;
+		product.description = req.body.description || product.description;
+		product.brand = req.body.brand || product.brand;
+		product.category = req.body.category || product.category;
+		product.countInStock = req.body.countInStock || product.countInStock;
+
+		const updatedProduct = await product.save();
+
+		res.json(updatedProduct);
+	} else {
+		res.status(404);
+		throw new Error("Product not found");
+	}
+});
+
+export {
+	getProductById,
+	getProducts,
+	deleteProduct,
+	createProduct,
+	updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
 import {
+	createProduct,
 	deleteProduct,
 	getProductById,
 	getProducts,
+	updateProduct,
 } from "../controllers/productController.js";
 import { isAdmin, protectRoute } from "../middleware/authMiddleware.js";
 
@@ -11,6 +13,11 @@ import { isAdmin, protectRoute } from "../middleware/authMiddleware.js";
 // @route GET /api/products
 // @access Public
 router.route("/").get(getProducts);
+
+// @desc Create a product
+// @route POST /api/products/
+// @access Private/Admin
+router.route("/").post(protectRoute, isAdmin, createProduct);
 
 // @desc Fetch one product
 // @route GET /api/products/:id
@@ -21,5 +28,10 @@ router.route("/:id").get(getProductById);
 // @route DELETE /api/products/:id
 // @access Private/Admin
 router.route("/:id").delete(protectRoute, isAdmin, deleteProduct);
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+router.route("/:id").put(protectRoute, isAdmin, updateProduct);
 
 export default router;

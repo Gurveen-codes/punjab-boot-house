44ac95bbccd2d9e0076b73648f2dc486c846165c
Gurveen <gurveens11@gmail.com>
2021-04-28 14:58:17 +0530

    [F]: Change Product Card

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,32 +4,32 @@ import { Card } from "react-bootstrap";
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
-  return (
-    <Card className="my-3 p-3 rounded">
-      <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top"></Card.Img>
-      </Link>
+	return (
+		<Card className="my-3 p-3 rounded">
+			<Link to={`/product/${product._id}`}>
+				<Card.Img src={product.image} variant="top"></Card.Img>
+			</Link>
 
-      <Card.Body>
-        <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
-            <strong>{product.name}</strong>
-          </Card.Title>
-        </Link>
-      </Card.Body>
+			<Card.Body>
+				<Link to={`/product/${product._id}`}>
+					<Card.Title as="h5">
+						<strong>{product.name}</strong>
+					</Card.Title>
+				</Link>
+			</Card.Body>
 
-      <Card.Text as="div">
-        <div className="my-3">
-          <Rating
-            value={product.rating}
-            text={`  ${product.numReviews} reviews`}
-          ></Rating>
-        </div>
-      </Card.Text>
+			<Card.Text as="div">
+				<div className="my-3">
+					<Rating
+						value={product.rating}
+						text={`  ${product.numReviews} reviews`}
+					></Rating>
+				</div>
+			</Card.Text>
 
-      <Card.Text as="h3">Rs.{product.price}</Card.Text>
-    </Card>
-  );
+			<Card.Text as="h4">Rs.{product.price}</Card.Text>
+		</Card>
+	);
 };
 
 export default Product;
